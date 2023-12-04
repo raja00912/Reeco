55174856c2d7980cfbe8aa832f77edf5cf702d4e
raja00912cd <mdramiz025@gmail.com>
2023-12-04 12:00:40 +0530

    created search bar

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import Belownav from './components/belownav';
+import Searchbar from './components/searchbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Belownav />
+      <Searchbar />
     </div>
   );
 }
---
+++ myapp/src/components/searchbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Searchbar() {
+    return (
+        <div id='searchbar'>
+            <input type='text' placeholder='Search....' />
+            <button>Add item</button>
+        </div>
+    )
+}
+
+export default Searchbar
