b3385cce6bc8168ac215cb64e08988c74bee9d21
raja00912cd <mdramiz025@gmail.com>
2023-12-04 11:52:08 +0530

    created order bar

---
+++ myapp/src/components/belownav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './navbarstyle.css'
+
+function Belownav() {
+    return (
+        <div id='belownav'>
+            <div>Orders - Order 32457ABC</div>
+            <span>Order 32457ABC</span>
+            <button>Back</button>
+            <button>Approve Order</button>
+        </div>
+    )
+}
+
+export default Belownav
